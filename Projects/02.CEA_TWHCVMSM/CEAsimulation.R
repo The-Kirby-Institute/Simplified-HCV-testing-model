a0c3b418c3f37d542d550308652d99a11dc5ae80
ninowwss <wuhj527@gmail.com>
2023-09-03 11:54:16 +1000

    change data and code path

--- Projects/02.CEA_TWHCVMSM/CEAsimulation.R
+++ Projects/02.CEA_TWHCVMSM/CEAsimulation.R
@@ -17,45 +17,64 @@ library(doParallel)
 # we specify the number of cores/workers we want to use
 registerDoParallel(cores = detectCores() - 1)
 
+codep <- "/Users/jjwu/Library/CloudStorage/OneDrive-UNSW/05. PhD Project/TWHCV-model"
+epidatapath <- "/Users/jjwu/Library/CloudStorage/OneDrive-UNSW/05. PhD Project/Taiwan-MSM-HCV-model"
 #file path of "TWHCV-model" project
-codepath <- file.path(here() %>% dirname(), 'TWHCV-model/03. Code/Functions')
+Rcode <- file.path(codep, '03. Code')
+scenariopath <- file.path(epidatapath, '01. DATA/model input/Scenarios')
 
-DataFolder <- file.path(here(), "01. DATA/model input")
-
+# save and cost data folder 
+dtp <- "/Users/jjwu/Library/CloudStorage/OneDrive-UNSW/05. PhD Project/Simplified HCV testing model_/Projects/02.CEA_TWHCVMSM"
+DataFolder <- file.path(dtp, "01. DATA/model input")
+outputdt <- file.path(dtp, "02. Output/RDA")
 # Rda file path 
 # load the .rda file of base estimate 
-rdapath <- file.path(here()%>%dirname(), "Taiwan-MSM-HCV-model")
+
+
+load(file.path(epidatapath , paste0("HCVModel", "param",".rda")))
 
 
 
-projectFile <- file.path(rdapath , paste0("HCVModel",".rda"))
+
+# source 
+source(file.path(Rcode, "Functions/HCV_model.R"))
+
+source(file.path(Rcode, "Functions/plotFunctions.R"))
+
+source(file.path(Rcode, "Functions/plotOptions.R"))
+
+source(file.path(Rcode, "Functions/Scenarios_wrangling.R"))
+
+
+
+projectFile <- file.path(epidatapath , paste0("HCVModel",".rda"))
 
 projectVars <- load(projectFile)
 
-load(file.path(rdapath , paste0("HCVModel", "cali",".rda")))
+load(file.path(epidatapath , paste0("HCVModel", "cali",".rda")))
 
-load(file.path(here("02. Output/RDA"), "cost.rda"))
+load(file.path(outputdt, "cost.rda"))
 
-load(file.path(here("02. Output/RDA"), "Param_dfExt.rda"))
+load(file.path(outputdt, "Param_dfExt.rda"))
 
-load(file.path(here("02. Output/RDA"), "mainS_PrEPHIV.rda"))
+load(file.path(outputdt, "mainS_PrEPHIV.rda"))
 
-load(file.path(here("02. Output/RDA"), "toft.rda"))
+load(file.path(outputdt, "toft.rda"))
 # output file path 
 # dir.create("02. Output") # create subdircetory 
 # dir.create("02. Output/RDA") 
 # dir.create("02. Output/Results")
-ResultsFolder <- file.path(here(), "02. Output/Results")
-outputdt <- here("02. Output/RDA")
+ResultsFolder <- file.path(dtp, "02. Output/Results")
+
 
 # source 
-source(file.path(codepath, "HCV_model.R"))
+source(file.path(Rcode, "Functions/HCV_model.R"))
 
-source(file.path(codepath, "plotFunctions.R"))
+source(file.path(Rcode, "Functions/plotFunctions.R"))
 
-source(file.path(codepath, "plotOptions.R"))
+source(file.path(Rcode, "Functions/plotOptions.R"))
 
-source(file.path(codepath, "Scenarios_wrangling.R"))
+source(file.path(Rcode, "Functions/Scenarios_wrangling.R"))
 
 currTime <- format(Sys.time(), "%Y-%m-%d_%H-%M-%S")
 
@@ -96,7 +115,7 @@ for(i in names(pop_arrayPrEPHIV)){
 # Run sampled parameter sets
 HCV$numberSamples <- 1000
 
-load(file.path(rdapath , paste0("HCVModel", "param",".rda")))
+load(file.path(epidatapath , paste0("HCVModel", "param",".rda")))
 
 
 # trim the time points 
