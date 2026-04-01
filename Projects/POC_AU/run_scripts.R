f5ded0e246c0aef2163302560f84164961f7e964
ninowwss <wuhj527@gmail.com>
2026-04-01 17:11:05 +1100

    Add run_scripts.R

---
+++ Projects/POC_AU/run_scripts.R
@@ -0,0 +1,29 @@
+#### run scripts #### 
+
+# after running {scenario set up_20260122_scaledscaleup.R}
+
+source("~/Projects/Simplified-HCV-testing-model/Projects/POC_AU/02_3. Uncertainty.R", echo = TRUE)
+rm(list = ls())
+gc()
+source("~/Projects/Simplified-HCV-testing-model/Projects/POC_AU/02_3_1. Scenario_uncertainty.R", echo = TRUE)
+rm(list = ls())
+gc()
+source("~/Projects/Simplified-HCV-testing-model/Projects/POC_AU/02_4. Param_sim.R", echo = TRUE)
+rm(list = ls())
+gc()
+source("~/Projects/Simplified-HCV-testing-model/Projects/POC_AU/02_4. Param_sim_scenario.R", echo = TRUE)
+rm(list = ls())
+gc()
+
+source("~/Projects/Simplified-HCV-testing-model/Projects/POC_AU/Resultsummary.R", echo = TRUE)
+rm(list = ls())
+gc()
+
+source("~/Projects/Simplified-HCV-testing-model/Projects/POC_AU/Resultsummary_sensitivity.R", echo = TRUE)
+rm(list = ls())
+gc()
+
+source("~/Projects/Simplified-HCV-testing-model/Projects/POC_AU/Res_aggregate_sensitivity.R", echo = TRUE)
+rm(list = ls())
+gc()
+
